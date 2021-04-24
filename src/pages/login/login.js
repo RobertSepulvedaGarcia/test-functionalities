60b8d0fe28e740b8b0ea36c31664b57e6c789a96
TekkenPlayer <robert.sepulveda1995@gmail.com>
2021-04-24 03:10:47 -0400

    made responsive design and fix some stuffs

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import { Card, Form, Button } from 'react-bootstrap';
+import { Card, Form, Button, Row, Col } from 'react-bootstrap';
 import Layout from './Layout';
+import './login.scss';
 const login = () => {
   const { Body, Header } = Card;
   const { Group, Label, Control, Text } = Form;
 
   return (
     <Layout>
-      <Card>
+      <Card className="card-login">
         <Header>
-          <p className='display-3'>Login</p>
+          <p className="display-3 text-center">Login</p>
         </Header>
         <Body>
           <Form>
-            <Group controlId='formBasicEmail'>
+            <Group controlId="formBasicEmail">
               <Label>Email address</Label>
-              <Control type='email' placeholder='Enter email' />
-              <Text className='text-muted'>
+              <Control type="email" placeholder="Enter email" />
+              <Text className="text-muted">
                 We'll never share your email with anyone else.
               </Text>
             </Group>
 
-            <Group controlId='formBasicPassword'>
+            <Group controlId="formBasicPassword">
               <Label>Password</Label>
-              <Control type='password' placeholder='Password' />
+              <Control
+                type="password"
+                placeholder="Password"
+                autocomplete="on"
+              />
             </Group>
-
-            <Button variant='primary' type='submit'>
-              Submit
-            </Button>
+            <Row style={{ marginTop: '50px' }}>
+              <Col sm={12} lg={6} className="d-flex justify-content-center">
+                <Button variant="outline-primary" type="submit">
+                  Submit
+                </Button>
+              </Col>
+              <Col sm={12} lg={6} className="d-flex justify-content-center">
+                <Button variant="outline-secondary">Create Account</Button>
+              </Col>
+            </Row>
           </Form>
         </Body>
       </Card>
